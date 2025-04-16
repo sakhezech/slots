a7e3d154c3b908684003eeb91fe7436f21fc76a1
sakhezech <contact@sakhezech.ru>
2025-04-16 23:21:38 +0300

    feat: working hardcoded slots

---
+++ src/slots/__init__.py
@@ -0,0 +1,85 @@
+import random
+import time
+
+
+class Column:
+    def __init__(self, spins: int, start_idx: int = 0) -> None:
+        self.chars: list[str] = []
+        # HACK: hardcoded characters
+        # get a list of characters, paddings, and separators here
+        # and construct the list
+        for char in range(0, 10):
+            self.chars.extend(
+                [
+                    '-----',
+                    '|   |',
+                    f'| {char} |',
+                    '|   |',
+                ]
+            )
+        self.char_size = 3
+        self.sep_size = 1
+        self.num_of_chars = 10
+
+        self.count = 0
+
+        self.offset = self.char_size + self.sep_size
+        self.idx = start_idx * self.offset
+        self.max_idx = self.num_of_chars * self.offset
+        self.steps_to_take = spins * self.offset
+
+    def get_frame(self) -> tuple[list[str], bool]:
+        start = self.idx
+        end = self.idx + self.offset + self.sep_size
+
+        if end > len(self.chars):
+            frame = self.chars[start:] + self.chars[: end - len(self.chars)]
+        else:
+            frame = self.chars[start:end]
+
+        done = True
+        if self.count < self.steps_to_take:
+            done = False
+            self.count += 1
+
+            self.idx += 1
+            if self.idx > self.max_idx:
+                self.idx = 1
+
+        return frame, done
+
+
+class Slots:
+    def __init__(
+        self,
+        num_of_columns: int = 3,
+        seed: int | str | bytes | bytearray | None = None,
+    ) -> None:
+        self.num_of_columns = num_of_columns
+        self.rand = random.Random(seed)
+        self.columns: list[Column] = []
+
+        spin_num = 0
+        for _ in range(self.num_of_columns):
+            # HACK: arbitrary numbers
+            spin_num += self.rand.randint(5, 10)
+            self.columns.append(Column(spin_num))
+
+    def spin(self) -> None:
+        while True:
+            data = [column.get_frame() for column in self.columns]
+            frames = [frame_and_status[0] for frame_and_status in data]
+            statuses = [frame_and_status[1] for frame_and_status in data]
+
+            height = len(frames[0])
+            main_frame = [
+                ''.join([char_lines[idx] for char_lines in frames])
+                for idx in range(height)
+            ]
+
+            print('\n'.join(main_frame))
+            if all(statuses):
+                break
+            # HACK: arbitrary time
+            time.sleep(0.1)
+            print(f'\x1b[{height}A\r\x1b[J', end='')
